003cac5c1628541d04351d47b71c792af34e6043
Jaswanthkota <kotajaswanth2@gmail.com>
2024-09-19 15:59:21 +0530

    Create Jashu7.c

---
+++ week2/Jashu7.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#include<math.h>
+
+int main()
+
+{
+
+int a,b,c;
+
+float s, area;
+
+printf("enter a,b,c value");
+
+scanf("%i%i%i",&a,&b,&c);
+
+s=(a+b+c)/2;
+
+area=sqrt(s*(s-a)*(s-b)*(s-c));
+
+printf("%i",агеа);
+
+}
