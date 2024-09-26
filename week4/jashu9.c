6a934c207919ca641148e1aeb8e382ea90d9d5db
Jaswanthkota <kotajaswanth2@gmail.com>
2024-09-26 15:30:45 +0530

    Create jashu9.c

---
+++ week4/jashu9.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f,g,x;
+printf("enter a,b,c,d,e,f,g values");
+scanf("%i%i%i%i%i%i%i",&a,&b,&c,&d,&e,&f,&g);
+x=a+b*c+(d*e)+f*g;
+printf("%i",x);
+}
+
